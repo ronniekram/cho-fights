1571ee4e5ba7fcd4506e62c0d7a8360df23463be
ronnie bee <60586619+ronniekram@users.noreply.github.com>
2022-12-12 21:05:12 -0500

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect, useState } from "react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "twin.macro";
+
+import GlobalStyles from "../styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  /** Prevents hydration error introduced by React 18. */
+  const [showChild, setShowChild] = useState<boolean>(false);
+
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === `undefined`) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>CHORIZO FIGHTS!!!</title>
+      </Head>
+      <GlobalStyles />
+      <div tw="antialiased">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+};
